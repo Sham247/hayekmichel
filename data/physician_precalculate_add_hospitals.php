03ef436ed149955308635f7cf66837b964134195
Jay Volk <jayvolk@Jays-MacBook-Pro.local>
2019-05-16 07:32:07 -0400

    quality data is in place, efficiency is still fake

--- data/physician_precalculate_add_hospitals.php
+++ data/physician_precalculate_add_hospitals.php
@@ -13,37 +13,39 @@
   } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
   }
+    for($i=1;$i<=5;$i++) {
+      $data = $pdo->query("select distinct `hospital_affiliation" . $i . "`  from _phys_measures_precalc")
+        ->fetchAll();
+      foreach ($data as $d) {
+        if ($d['hospital_affiliation' . $i]) {
+          echo '<h2>HOSPITAL ID: ' . $d['hospital_affiliation' . $i] . '</h2>';
+          for ($j = 1; $j <= 13; $j++) {
+            $dataweight = $pdo->query("SELECT category_weight_pct from _hospital_quality_cats_weight where id = " . $j)
+              ->fetchAll();
+            foreach ($dataweight as $w) {
+              $weight = ($w['category_weight_pct'] / 100);
+            }
+            if ($j == 1) {
+              $total = 0;
+            }
 
-    $data = $pdo->query("select distinct hospital_affiliation1  from _phys_measures_precalc")->fetchAll();
-    foreach ($data as $d) {
-      if($d['hospital_affiliation1']) {
-        echo '<h2>HOSPITAL ID: ' . $d['hospital_affiliation1'] . '</h2>';
-        for ($i = 1; $i <= 13; $i++) {
-          $dataweight = $pdo->query("SELECT category_weight_pct from _hospital_quality_cats_weight where id = " . $i)
-            ->fetchAll();
-          foreach ($dataweight as $w) {
-            $weight = ($w['category_weight_pct'] / 100);
-          }
-          if ($i == 1) {
-            $total = 0;
+            $data2 = $pdo->query("select count(percentile) as count, sum(percentile) as tot from _hospital_quality_results where provider_id = " . $d['hospital_affiliation' . $i] . " and quality_category = " . $j)
+              ->fetchAll();
+            foreach ($data2 as $d2) {
+              echo '<br>CATEGORY ' . $j . ' - SUM = ' . $d2['tot'] . 'WEIGHTED: ' . ($d2['tot'] * $weight) . '<hr>';
+              $total = $total + ($d2['tot'] * $weight);
+            }
           }
 
-          $data2 = $pdo->query("select count(percentile) as count, sum(percentile) as tot, (sum(percentile) / count(percentile)) as avg from _hospital_quality_results where provider_id = " . $d['hospital_affiliation1'] . " and quality_category = " . $i)
-            ->fetchAll();
-          foreach ($data2 as $d2) {
-            echo '<br>CATEGORY ' . $i . ' - AVG = ' . $d2['avg'] . 'WEIGHTED: ' . ($d2['avg'] * $weight) . '<hr>';
-            $total = $total + ($d2['avg'] * $weight);
-          }
+          $datac = [
+            'total' => intval($total),
+            'hospital_affiliation' . $i => $d['hospital_affiliation' . $i],
+          ];
+          $sqlc = "UPDATE _phys_measures_precalc set hospital_percentile" . $i . " = :total where hospital_affiliation" . $i . " = :hospital_affiliation" . $i;
+          echo $sqlc . '<hr>';
+          $stmt = $pdo->prepare($sqlc);
+          $stmt->execute($datac);
+          echo '<h1>TOTAL VALUE = ' . $total . '</h1>';
         }
-
-        $datac = [
-          'total' => intval($total),
-          'hospital_affiliation1' => $d['hospital_affiliation1'],
-        ];
-        $sqlc = "UPDATE _phys_measures_precalc set hospital_percentile1 = :total where hospital_affiliation1 = :hospital_affiliation1";
-        echo $sqlc . '<hr>';
-        $stmt = $pdo->prepare($sqlc);
-        $stmt->execute($datac);
-        echo '<h1>TOTAL VALUE = ' . $total . '</h1>';
       }
     }
